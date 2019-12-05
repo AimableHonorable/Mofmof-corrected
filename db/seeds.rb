8adc1ac6e142a15e2c2c03b5be8efc33d2b4c7ef
AimableHonorable <aimatwiri@gmail.com>
2019-12-05 11:16:06 +0200

    Corrected form

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Property.create(property: 'House', rent:50000, address: 'Kigal', building_age: 20, remarks: 'Living house')
